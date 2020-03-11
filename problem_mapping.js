75452289cf38fd7ce0e39dc534535b5b79853ff9
tyang0119 <tianhao@wustl.edu>
2020-03-10 23:34:05 -0700

    delete linked list node

--- problem_mapping.js
+++ problem_mapping.js
@@ -768,7 +768,7 @@ problem_mapping = {
                 "total": 512
             },
             "Python: delete_node_from_list.py": {
-                "passed": 0,
+                "passed": 512,
                 "total": 512
             }
         },
