3c43f0e01dabd0d4a35c88197772fce1fb77738c
tyang0119 <tianhao@wustl.edu>
2020-03-14 15:11:01 -0700

    update has_path_sum

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -3,8 +3,12 @@ from test_framework import generic_test
 
 
 def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return False
+    if not tree.left and not tree.right:
+        return tree.data == remaining_weight
+
+    return (has_path_sum(tree.left, remaining_weight - tree.data) or has_path_sum(tree.right, remaining_weight - tree.data))
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -1080,7 +1080,7 @@ problem_mapping = {
                 "total": 7690
             },
             "Python: path_sum.py": {
-                "passed": 0,
+                "passed": 7690,
                 "total": 7690
             }
         },
